868f43613a0a55710aab821e00498da25ee7d557
VladStorozhenko <43917096+VladStorozhenko@users.noreply.github.com>
2020-08-06 23:07:33 +0300

    Updates

---
+++ precache-manifest.0459d231e50dc77bbaa9b9010beb77a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466eecc58b8089673e7bca5b18933b7d",
+    "url": "/VladStorozhenko/reactquizapp.git/index.html"
+  },
+  {
+    "revision": "3e3b28cd4301577af441",
+    "url": "/VladStorozhenko/reactquizapp.git/static/css/main.f0358a99.chunk.css"
+  },
+  {
+    "revision": "0c7345d95f4cc8749964",
+    "url": "/VladStorozhenko/reactquizapp.git/static/js/2.15c077ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VladStorozhenko/reactquizapp.git/static/js/2.15c077ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3b28cd4301577af441",
+    "url": "/VladStorozhenko/reactquizapp.git/static/js/main.5ba4aaf0.chunk.js"
+  },
+  {
+    "revision": "92a81eeaff01de5672b2",
+    "url": "/VladStorozhenko/reactquizapp.git/static/js/runtime-main.53044576.js"
+  }
+]);
